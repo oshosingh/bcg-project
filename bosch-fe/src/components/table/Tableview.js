855b6721dd17c2a830b4c2928f96aed76fb56545
osho singh <oshosingh25@gmail.com>
2022-08-24 14:12:48 +0000

    improved condition to remove spinner component

--- bosch-fe/src/components/table/Tableview.js
+++ bosch-fe/src/components/table/Tableview.js
@@ -7,25 +7,16 @@ import Spinner from 'react-bootstrap/Spinner';
 function Tableview(props) {
 
     const [tableData, setTableData] = useState(null)
-    const [isLoading, setIsLoading] = useState(true)
  
     useEffect(() => {
         getAllSales().then((data) => {
           setTableData(data)
         })
-
-        // setTimeout(() => {
-        //     setTableData([])
-        // }, 5000)
-
-        if(tableData !== null) {
-          setIsLoading(false)
-        }
     }, [])
     
     return (
       <div className='parent'>
-        { isLoading === false ? <h1>'hello world'</h1> : <Spinner animation="border" className='spinner'/> }
+        { tableData !== null ? <h1>'hello world'</h1> : <Spinner animation="border" className='spinner'/> }
       </div>
     )
 }
