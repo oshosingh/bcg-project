8d476c504c9dfe14cd9e5fc97ade2abd260f43b6
osho singh <oshosingh25@gmail.com>
2022-08-24 14:46:45 +0000

    added table component

--- bosch-fe/src/components/table/Tableview.js
+++ bosch-fe/src/components/table/Tableview.js
@@ -3,20 +3,42 @@ import './tableview.css'
 import { useState } from 'react'
 import { getAllSales } from '../../RestApi'
 import Spinner from 'react-bootstrap/Spinner';
+import Table from 'react-bootstrap/Table';
 
 function Tableview(props) {
 
     const [tableData, setTableData] = useState(null)
+    const tableColumns = ['Sales id', 'Date of Purchase', 'Customer Id', 'Vehicle Segment', 'Selling Price',
+                          'Customer Gender', 'Customer Income Group', 'Customer Region' ]
  
     useEffect(() => {
         getAllSales().then((data) => {
           setTableData(data)
         })
     }, [])
+
+
+    const getTableComponent = () => {
+      return (
+        <div className='table-view'>
+          <Table striped bordered hover variant="dark">
+              <thead>
+                <tr>
+                    {tableColumns.map((element, index) => {
+                        return (
+                            <th key={index}>{element}</th>
+                        )
+                    })}
+                </tr>
+              </thead>
+          </Table>
+        </div>
+      )
+    }
     
     return (
       <div className='parent'>
-        { tableData !== null ? <h1>'hello world'</h1> : <Spinner animation="border" className='spinner'/> }
+        { tableData !== null ? getTableComponent() : <Spinner animation="border" className='spinner'/> }
       </div>
     )
 }
