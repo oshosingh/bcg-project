016142b58394340d9ede8a7e43a1aaeab7128e24
osho singh <oshosingh25@gmail.com>
2022-08-25 14:12:52 +0000

    changed update api call logic

--- bosch-fe/src/App.js
+++ bosch-fe/src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   useEffect(() => {
       console.log('found change in search input')
-  }, [searchInputData])
+  }, [searchInputData, action])
 
   const renderTableView = (action) => {
       setAction(action)
